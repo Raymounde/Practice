26401489c30ec83f777d2d4547dd42b6c13386ad
Raymounde <a0falcon@gmail.com>
2017-04-06 16:07:50 +0800

    initial

---
+++ JavaFx/Practice_001/src/Main.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Created by Alfie on 4/6/2017.
+ * This is a JavaFX practice based on Tutorial 1 from New Boston
+ */
+public class Main extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
